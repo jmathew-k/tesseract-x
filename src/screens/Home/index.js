ad867120c201c862167ef219b3ae395abc66cf41
Joshua Mathew <jm@Joshuas-iMac.local>
2022-11-18 16:48:59 +0530

    null user

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -9,26 +9,29 @@ import RefundRequests from "../../components/RefundRequests";
 import ProTips from "./ProTips";
 import MoreCustomers from "./MoreCustomers";
 import ProductViews from "./ProductViews";
+import { useAuth } from '../../contexts/AuthContext'
 
 const Home = () => {
+  const { currentUser } = useAuth()
   return (
-    <>
-      <div className={styles.row}>
-        <div className={styles.col}>
-          <Overview className={styles.card} />
-          <ProductViews className={styles.card} />
-          <ProTips className={styles.card} />
-          <MoreCustomers />
-        </div>
-        <div className={styles.col}>
-          <PopularProducts className={styles.card} views="4" />
-          <Comments className={styles.card} />
-          <RefundRequests title="Refund requests" classTitle="title-red" />
-        </div>
-      </div>
-      <TooltipGlodal />
-    </>
-  );
+		<>
+			<text>{`The current user is : ${currentUser}`}</text>
+			<div className={styles.row}>
+				<div className={styles.col}>
+					<Overview className={styles.card} />
+					<ProductViews className={styles.card} />
+					<ProTips className={styles.card} />
+					<MoreCustomers />
+				</div>
+				<div className={styles.col}>
+					<PopularProducts className={styles.card} views='4' />
+					<Comments className={styles.card} />
+					<RefundRequests title='Refund requests' classTitle='title-red' />
+				</div>
+			</div>
+			<TooltipGlodal />
+		</>
+	)
 };
 
 export default Home;
